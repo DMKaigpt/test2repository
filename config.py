2b61c9a4b3b464292b06bdeea01679cd1234187e
DMKaigpt <largussimplex@gmail.com>
2024-09-11 20:15:49 +0800

    Create config.py
    первый нах

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+
+TELEGRAM_BOT_TOKEN = 'YOUR_TOKEN'
+OPENAI_API_KEY = 'YOUR_API'
+GPT_MODEL = 'gpt-4o-mini'
+
+# Настройки прокси
+PROXY_HOST = '1'
+PROXY_PORT = '1'
+PROXY_USER = '1'
+PROXY_PASS = '1'
+
+1 строка
+
+3 строка
+149 строка
